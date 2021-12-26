01f6613935c89cab196abaf8ddd8c581d6bc9756
hsunAlfred <chenghsungt@gmail.com>
2021-12-27 00:33:18 +0800

    NLP model select done

--- nlp_code/nlp_ml_predict.py
+++ nlp_code/nlp_ml_predict.py
@@ -1,3 +1,4 @@
+from typing import final
 from joblib import load
 from nlp_frame import nlp_frame
 
@@ -11,7 +12,7 @@ class nlp_ml_predict(nlp_frame):
         vect = load(vectPath)
 
         tex = self.seg(predictList, h, u)
-        tex = vect.fit_transform(tex).toarray()
+        tex = vect.transform(tex).toarray()
 
         res = model.predict(tex)
 
@@ -21,12 +22,20 @@ class nlp_ml_predict(nlp_frame):
 if __name__ == "__main__":
     nmp = nlp_ml_predict()
     params = {
-        "modelPath": 'nlpModel_NB/nlp_NB_HMM_True_paddle_False.joblib',
-        "vectPath": 'nlpModel_NB/nlp_vect_HMM_True_paddle_True.vect',
-        "predictList": ["青椒好難吃", "番茄很棒"],
-        "h": False,
+        "modelPath": 'nlpModel_NB/1640536039.7400753.joblib',
+        "vectPath": 'nlpModel_NB/vect_1640536039.7400753.vect',
+        "predictList": ["青椒也太難吃", "番茄超級好吃"],
+        "h": True,
         "u": False
     }
 
     tex, res = nmp.predict(**params)
-    print(params['predictList'], res)
+    final_res = []
+    for r in res:
+        if r>=4:
+            final_res.append(1)
+        elif r == 3:
+            final_res.append(0)
+        elif r <= 2:
+            final_res.append(-1)
+    print(params['predictList'], final_res)
