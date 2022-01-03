d166f87f772682b8a81e24b3df5afdb0ddcc4597
hsunAlfred <chenghsungt@gmail.com>
2022-01-03 22:32:32 +0800

    corpus new2

--- nlp_code/nlp_ml_training.py
+++ nlp_code/nlp_ml_training.py
@@ -148,7 +148,7 @@ def ml_call(vectParams, segParams, modelSelect, modelParams, resultTimestamp, du
 
 
 if __name__ == "__main__":
-    # start = time.time()
+    start = time.time()
 
     # try:
     #     resultTimestamp = f"{time.time()}"
@@ -185,49 +185,49 @@ if __name__ == "__main__":
 
     # end1 = time.time()
 
-    # print(f'NB time:{(end1-start):.3f}')
+    print(f'NB time:{(end1-start):.3f}')
     # max_df min_df -> float in range [0.0, 1.0]
-    # start = time.time()
-    # nnNB = 1
-    # for ma in range(1, 11):
-    #     for h, u in ((True, True), (True, False)):
-    #         for b in (True, False):
-    #             for a in ("char", "char_wb"):
-    #                 try:
-    #                     print(
-    #                         f'***************iteration:{nnNB}***************')
-    #                     resultTimestamp = f"{time.time()}"
+    start = time.time()
+    nnNB = 1
+    for ma in range(1, 11):
+        for h, u in ((True, True), (True, False)):
+            for b in (True, False):
+                for a in ("char", "char_wb"):
+                    try:
+                        print(
+                            f'***************iteration:{nnNB}***************')
+                        resultTimestamp = f"{time.time()}"
 
-    #                     vectParams = {
-    #                         "analyzer": a,
-    #                         "max_df": ma/10,
-    #                         "min_df": 0.0,
-    #                         "binary": b
-    #                     }
+                        vectParams = {
+                            "analyzer": a,
+                            "max_df": ma/10,
+                            "min_df": 0.0,
+                            "binary": b
+                        }
 
-    #                     segParams = {
-    #                         "corpus": "./corpus_words/corpus_new.xlsx",
-    #                         "HMM": h,
-    #                         "use_paddle": u
-    #                     }
+                        segParams = {
+                            "corpus": "./corpus_words/corpus_new.xlsx",
+                            "HMM": h,
+                            "use_paddle": u
+                        }
 
-    #                     modelSelect = "NB"
+                        modelSelect = "NB"
 
-    #                     # alpha:Additive (Laplace/Lidstone) smoothing parameter(0 for no smoothing).
-    #                     # "fit_prior": bool, default = True Whether to learn class prior probabilities or not. If false, a uniform prior will be used.
-    #                     modelParams = {
-    #                         "alpha": 1.0,
-    #                         "fit_prior": True
-    #                     }
+                        # alpha:Additive (Laplace/Lidstone) smoothing parameter(0 for no smoothing).
+                        # "fit_prior": bool, default = True Whether to learn class prior probabilities or not. If false, a uniform prior will be used.
+                        modelParams = {
+                            "alpha": 1.0,
+                            "fit_prior": True
+                        }
 
-    #                     ml_call(vectParams, segParams,
-    #                             modelSelect, modelParams, resultTimestamp)
-    #                 except Exception as e:
-    #                     temp = f'\n{nnNB}\n{e}\n{resultTimestamp}\n{vectParams}\n{segParams}\n{modelSelect}\n{modelParams}'
-    #                     with open('./info/err.txt', 'a', encoding='utf-8') as f:
-    #                         f.write(temp)
-    #                     print(temp)
-    #                 nnNB += 1
+                        ml_call(vectParams, segParams,
+                                modelSelect, modelParams, resultTimestamp)
+                    except Exception as e:
+                        temp = f'\n{nnNB}\n{e}\n{resultTimestamp}\n{vectParams}\n{segParams}\n{modelSelect}\n{modelParams}'
+                        with open('./info/err.txt', 'a', encoding='utf-8') as f:
+                            f.write(temp)
+                        print(temp)
+                    nnNB += 1
 
     end1 = time.time()
 
@@ -291,8 +291,8 @@ if __name__ == "__main__":
                                 nnRF += 1
     end2 = time.time()
 
-    # print(f'NB time:{(end1-start):.3f}')
-    # print(f'NB iternations:{nnNB}')
+    print(f'NB time:{(end1-start):.3f}')
+    print(f'NB iternations:{nnNB}')
 
     print(f'RF time:{(end2-end1):.3f}')
     print(f'RF iternations:{nnRF}')
